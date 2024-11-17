b921471455aad4f0ca7086c96c79cc2e9843ebc2
Niko Pavlinek <niko.pavlinek@gmail.com>
2024-11-17 11:51:00 +0100

    Fix build on Linux

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -ex
 clang \
     -DCONFIG_BIGNUM \
     -DCONFIG_VERSION=\"$(<quickjs/VERSION)\" \
+    -D_GNU_SOURCE \
     -o qjs_test \
+    -lm \
     -lpthread \
     quickjs/cutils.c \
     quickjs/libbf.c \
